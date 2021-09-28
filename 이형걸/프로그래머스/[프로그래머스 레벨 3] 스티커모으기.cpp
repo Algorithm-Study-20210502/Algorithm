504c664b9446e160e50a9f9d2923f182a11f0a72
LeeHyungGeol <rjf1138@naver.com>
2021-09-29 00:13:20 +0900

    2021-09-28 / 2018 summer,winter coding : 스티커모으기: 원형dp

--- 이형걸/프로그래머스/[프로그래머스 레벨 3] 스티커모으기.cpp
+++ 이형걸/프로그래머스/[프로그래머스 레벨 3] 스티커모으기.cpp
@@ -4,27 +4,29 @@
 
 using namespace std;
 
-typedef long long ll;
+int solution(vector<int> sticker) {
+    int answer = 0, size = sticker.size();
+    vector<int> dp(size, 0);
+    
+    if(size == 1) {
+        return sticker[0];
+    }
+    
+    dp[0] = sticker[0];
+    dp[1] = dp[0];
+    for(int i = 2; i < size-1; ++i) {
+        dp[i] = max(dp[i-1], dp[i-2]+sticker[i]);
+    }
+    dp[size-1] = dp[size-2];
+    int Max = *max_element(dp.begin(), dp.end());
 
-ll solution(vector<int> sticker) {
-    ll answer = 0, even = 0, odd = 0, size = sticker.size();
-    
-    for(int i = 0; i < size; ++i) {
-        if((i+1)%2) {
-            odd += (ll)sticker[i];
-        }
-        else {
-            even += (ll)sticker[i];
-        }
+    dp[0] = 0;
+    dp[1] = sticker[1];
+    for(int i = 2; i < size; ++i) {
+        dp[i] = max(dp[i-1], dp[i-2]+sticker[i]);
     }
     
-    if(size%2) {
-        answer = max(answer, max(even, odd-sticker[size-1]));
-        answer = max(answer, odd-sticker[0]);
-    }
-    else {
-        answer = max(answer, max(even, odd));
-    }
+    answer = max(Max, *max_element(dp.begin(), dp.end()));
     
     return answer;
 }
